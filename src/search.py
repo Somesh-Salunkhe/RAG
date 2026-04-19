35252fb7ae01635d84353f5dffe79796433fb4d7
Somesh Salunkhe <salunkhesomesh1602@gmail.com>
2026-04-19 16:05:59 +0200

    Search

--- src/search.py
+++ src/search.py
@@ -26,12 +26,12 @@ class RAGsearch:
             print(f"[INFO] Groq LLM initialized: {llm_model}")
         
 
-        def search_and_summarize(self, query: str, top_k: int = 5) -> str:
-            results = self.vectorstore.query(query, top_k)
-            texts = [r["metadata"].get("text", "") for r in results if r['metadata']]
-            context = "\n\n".join(texts)
-            if not context:
-                return "No relevant documents found"
-            prompt = f"""Summarize the following context for the query: '{query}'\n\nContext:\n\n{context}\n\nSummmary: """
-            response = self.llm.invoke([prompt])
-            return response.content
+    def search_and_summarize(self, query: str, top_k: int = 5) -> str:
+        results = self.vectorstore.query(query, top_k)
+        texts = [r["metadata"].get("text", "") for r in results if r['metadata']]
+        context = "\n\n".join(texts)
+        if not context:
+            return "No relevant documents found"
+        prompt = f"""Summarize the following context for the query: '{query}'\n\nContext:\n\n{context}\n\nSummmary: """
+        response = self.llm.invoke([prompt])
+        return response.content
